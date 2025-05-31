be23e3b395f544a9e507fbefd172acccba6caa8f
rakib1234567-csl <rakib.ali.csl@gmail.com>
2025-05-31 07:14:26 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+// config.js
+
+module.exports = {
+  defaultLanguage: "bn",   // ডিফল্ট ভাষা: বাংলা (en = English, hi = Hindi)
+  timezone: "Asia/Dhaka",  // টাইমজোন: বাংলাদেশ
+  botName: "AR BOT",       // তোমার বটের নাম
+  ownerName: "RAKIB BOSS", // মালিকের নাম
+  autoReply: true,         // Auto reply চালু
+  prefix: "/",             // কমান্ড prefix
+  theme: "dark",           // বটের থিম
+  languageSupport: ["bn", "en", "hi"] // ভাষাসমূহ সাপোর্ট
+};
