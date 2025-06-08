ac1afc92ebc786fcc7971d1028e4749f2a2d7c4b
rakib1234567-csl <rakib.ali.csl@gmail.com>
2025-06-08 09:49:23 +0600

    Update gali_reply.js

--- Priyansh/commands/gali_reply.js
+++ Priyansh/commands/gali_reply.js
@@ -12,9 +12,9 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("fuck")==0 || event.body.indexOf("mc")==0 || event.body.indexOf("chod")==0 || event.body.indexOf("bal")==0 || event.body.indexOf("bc")==0 || event.body.indexOf("maa ki chut")==0 || event.body.indexOf("xod")==0 || event.body.indexOf("behen chod")==0 || event.body.indexOf("🖕")==0 || event.body.indexOf("madarchod")==0 || event.body.indexOf("chudi")==0 || event.body.indexOf("gala gali")==0) {
+	if (event.body.indexOf("fuck")==0 || event.body.indexOf("mc")==0 || event.body.indexOf("chod")==0 || event.body.indexOf("bal")==0 || event.body.indexOf("bc")==0 || event.body.indexOf("kuttar bacca")==0 || event.body.indexOf("xod")==0 || event.body.indexOf("madar chud")==0 || event.body.indexOf("🖕")==0 || event.body.indexOf("madarchod")==0 || event.body.indexOf("chudi")==0 || event.body.indexOf("magi")==0) {
 		var msg = {
-				body: "(Boss Dk , Gali q Dete Ho. Lund Katke Hath M rakh Dunga)",
+				body: "(Boss Dk , Gali delete kor naile kintu khobor ace koiya dilam)",
 			}
 			api.sendMessage(msg, threadID, messageID);
 		}
