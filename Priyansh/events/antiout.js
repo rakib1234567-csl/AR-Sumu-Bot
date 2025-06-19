ec0768e94d8cd0e351492504c98d4aef84c27832
rakib1234567-csl <rakib.ali.csl@gmail.com>
2025-06-19 20:19:33 +0600

    Update antiout.js

--- Priyansh/events/antiout.js
+++ Priyansh/events/antiout.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "antiout",
  eventType: ["log:unsubscribe"],
  version: "0.0.1",
- credits: "Rakib Boss",
+ credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
  description: "Listen events"
 };
 
@@ -15,8 +15,8 @@ module.exports.run = async({ event, api, Threads, Users }) => {
  if (type == "self-separation") {
   api.addUserToGroup(event.logMessageData.leftParticipantFbId, event.threadID, (error, info) => {
    if (error) {
-    api.sendMessage(`ইস ২য় বার এ্যাড করে দিতে পারলাম না 🥺 ${name} Group Mai :( `, event.threadID)
-   } else api.sendMessage(`বাগিয়া কই জাইতে, ${name} জানু, আমি তোমককে আবার এ্যাড করেদিলাম`, event.threadID);
+    api.sendMessage(`ইস ২য় বার এ্যাড করে দিতে পারলাম না 🥺 ${name}-কে গ্রুপে। :( `, event.threadID)
+   } else api.sendMessage(`বাগিয়া কই জাইতে, ${name} জানু, আমি তোমককে আবার এ্যাড করেদিলাম:( `, event.threadID);
   })
  }
 }
